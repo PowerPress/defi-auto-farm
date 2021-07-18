d8215104b795fb3ef597efee5d7f0ece470b2c97
Z8pn <Z8pn@users.noreply.github.com>
2021-07-18 05:38:36 +0200

    Update and rename creds.js to creds.sample.js

---
+++ creds.sample.js
@@ -0,0 +1,6 @@
+module.exports = {
+	privateKey: "", //used to interact with the wallet
+    	rpcUrl: "https://polygon-mainnet.infura.io/v3/APIKEY", // can be any other provider
+    	polygonscan: "APIKEY", //used for fetching contract abi
+    	discordWebHook: "https://discord.com/api/webhooks/",  //to print results
+}
